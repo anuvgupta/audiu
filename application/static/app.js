777d1b135ca982690ab8b7fedefc82088e4e7831
anuvgupta <me@anuv.me>
2022-05-12 00:06:51 -0500

    add particlejs bg

--- application/static/app.js
+++ application/static/app.js
@@ -28,80 +28,40 @@ var app = {
                     callback: callback,
                 });
             },
-            new_project: (_) => {
-                bootbox.confirm({
-                    centerVertical: true,
-                    title: '<span class="modal_title">Create Project</span>',
-                    message:
-                        `<div id='new_project_display_modal'>` +
-                        `<div style="margin: 3px 0;"><span class="modal_text_input_label">Name:</span>&nbsp;` +
-                        `<input placeholder="Project Zero" class="modal_text_input" id="np_modal_name_input" type='text' name='np_modal_name'/></div>` +
-                        `<div style="margin: 3px 0;"><span class="modal_text_input_label">Identifier:</span>&nbsp;` +
-                        `<input placeholder="project-zero" class="modal_text_input" id="np_modal_slug_input" type='text' name='np_modal_slug'/></div>` +
-                        `<div style="margin: 3px 0;"><span class="modal_text_input_label">Repository:</span>&nbsp;` +
-                        `<input placeholder="https://github.com/u/project-zero" class="modal_text_input" id="np_modal_repo_input" type='text' name='np_modal_repo'/></div>` +
-                        `<div style="margin: 3px 0;"><span class="modal_text_input_label">Description:</span>&nbsp;` +
-                        `<input placeholder="Lorem ipsum dolor sit amet..." class="modal_text_input" id="np_modal_desc_input" type='text' name='np_modal_desc'/></div>` +
-                        `<div style="height: 8px"></div></div>`,
-                    callback: (result) => {
-                        if (result) {
-                            var slug = `${$("#new_project_display_modal #np_modal_slug_input")[0].value
-                                }`.trim();
-                            var name = `${$("#new_project_display_modal #np_modal_name_input")[0].value
-                                }`.trim();
-                            var repo = `${$("#new_project_display_modal #np_modal_repo_input")[0].value
-                                }`.trim();
-                            var desc = `${$("#new_project_display_modal #np_modal_desc_input")[0].value
-                                }`.trim();
-                            if (slug == "" || name == "") return false;
-                            app.ws.api.new_project(slug, name, repo, desc);
-                        }
-                        return true;
-                    },
-                });
-            },
-            new_project_res: (message) => {
-                bootbox.confirm({
-                    centerVertical: true,
-                    title: '<span class="modal_title">Create Project</span>',
-                    message: `${message}`,
-                    callback: (result) => {
-                        if (result) {
-                            setTimeout((_) => {
-                                app.ws.api.get_projects();
-                            }, 100);
-                            return true;
-                        }
-                    },
-                });
-            },
         },
         place_root_block: (_) => {
             app.ui.block = Block("div", "app");
         },
+        load_particles_bg: next => {
+            particlesJS.load('particles-js', '/lib/particlesjs-config.json', next);
+        },
         init: (callback) => {
             app.ui.block.fill(document.body);
             Block.queries();
-            setTimeout((_) => {
-                if (app.main.config.target_run_id != '') {
-                    app.ui.block.data({
-                        target_run_id: app.main.config.target_run_id
-                    });
-                }
-                app.ui.block.css("opacity", "1");
-                app.ui.block.on("ready");
-                setTimeout(_ => {
-                    // enable ripple effect on material-ui buttons after generated and injected by block.js
-                    window.componentHandler.upgradeDom();
-                }, 100);
-            }, 100);
-            setTimeout((_) => {
-                Block.queries();
-                setTimeout((_) => {
-                    Block.queries();
-                }, 200);
+            setTimeout(_ => {
+                app.ui.load_particles_bg(_ => {
+                    setTimeout((_) => {
+                        if (app.main.config.target_run_id != '') {
+                            app.ui.block.data({
+                                target_run_id: app.main.config.target_run_id
+                            });
+                        }
+                        app.ui.block.css("opacity", "1");
+                        app.ui.block.on("ready");
+                        setTimeout(_ => {
+                            // enable ripple effect on material-ui buttons after generated and injected by block.js
+                            window.componentHandler.upgradeDom();
+                        }, 100);
+                    }, 100);
+                    setTimeout((_) => {
+                        Block.queries();
+                        setTimeout((_) => {
+                            Block.queries();
+                        }, 200);
+                    }, 50);
+                    callback();
+                });
             }, 50);
-            callback();
         },
     },
     web: {
